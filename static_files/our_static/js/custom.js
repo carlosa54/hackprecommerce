e1b8d442cf3d30dbcfba42c1c56eb75be772f6d9
carlosa54 <carlos_arg5@yahoo.com>
2016-10-16 08:42:14 -0400

    Temporarily upload static_files folder

---
+++ static_files/our_static/js/custom.js
@@ -0,0 +1,21 @@
+function showFlashMessage(message) {
+	// var template = "{% include 'alert.html' with message='" + message + "' %}"
+	var template = "<div class='container container-alert-flash'>" + 
+	"<div class='col-sm-3 col-sm-offset-8'> " + 
+	"<div class='alert alert-success alert-dismissible' role='alert'>" + 
+	"<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
+	"<span aria-hidden='true'>&times;</span></button>" 
+	+ message + "</div></div></div>"
+	$("body").prepend(template);
+	$(".container-alert-flash").fadeIn();
+	setTimeout(function(){ 
+		$(".container-alert-flash").fadeOut();
+
+	}, 1000);
+
+	setTimeout(function(){ 
+		$(".container-alert-flash").remove();
+
+	}, 1000);
+
+}
