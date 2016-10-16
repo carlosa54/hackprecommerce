978cc846395d190fed6df6a54cd7173c6ec5c0ff
carlosa54 <carlos_arg5@yahoo.com>
2016-10-16 01:25:41 -0400

    Added payment list and fix bug of cart total

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import ATHMovilCheckoutView
+from .views import ATHMovilCheckoutView,PaymentList
 
 urlpatterns = [
 	url(r'^$', ATHMovilCheckoutView.as_view(), name='athmovil'),
+	url(r'^payments/$', PaymentList.as_view(), name='payments'),
 ]
