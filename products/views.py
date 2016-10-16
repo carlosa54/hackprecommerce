71c8a8d0f337b5ef1c112f239bdad5270036a5d2
carlosa54 <carlos_arg5@yahoo.com>
2016-10-16 08:41:09 -0400

    Show payments history if the user is admin

--- products/views.py
+++ products/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Product
+from django.views.generic import CreateView
 
 # Create your views here.
 def home(request):
+	if str(request.user.is_superuser) == "True":
+		return redirect('payments')
 	return redirect('products')
 
 class ProductListView(ListView):
