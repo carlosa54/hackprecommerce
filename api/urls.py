01053d9d34b69092e4dd5334a412b0fdc38312a1
carlosa54 <carlos_arg5@yahoo.com>
2016-10-15 22:18:16 -0400

    Testing phone format for api request

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-
+from .views import ATHMovilCheckoutView
 
 urlpatterns = [
-    
+	url(r'^$', ATHMovilCheckoutView.as_view(), name='athmovil'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+import requests
 
 # Create your views here.
+def token_login():
+	payload = {"commUsername":"carlosa", "commPassword":"1234ab12"}
+	response = requests.get("http://athmapi.westus.cloudapp.azure.com/athm/requestSession", params=payload)
+	return response.json()["token"]
+
+class ATHMovilCheckoutView(TemplateView):
+	template_name = 'api/athmovil.html'
+
+	def get(self, request, *args, **kwargs):
+		token = token_login()
+
+		context = {
+			"token": token
+		}
+
+
+		template = self.template_name
+		return render(request,template,context)
+
+
