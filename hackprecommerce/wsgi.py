440c15783889e3e9785c2fcb5b4b7f8bbccf2639
Carlos Ramirez <carlosarg54@gmail.com>
2017-07-13 00:41:51 -0400

    Ezcart

--- hackprecommerce/wsgi.py
+++ hackprecommerce/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hackprecommerce project.
+WSGI config for ezcart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackprecommerce.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ezcart.settings")
 
 application = get_wsgi_application()
